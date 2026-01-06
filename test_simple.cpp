43c221913d5a4ab233d68bd4500c7b35bdaa81cc
goldfrad <sgold8822@gmail.com>
2026-01-07 00:23:19 +0200

    Clean up repository: keep only README.md, main.cpp, and src directory

--- test_simple.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "=== Testing C++ Output ===" << endl;
-    cout << "Hello from C++!" << endl;
-    cout << "Program is working correctly!" << endl;
-    return 0;
-}
